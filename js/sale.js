7c9f603dd4944e8988c1df04f130e22005d3e096
beanbeeean <beanbeeean@naver.com>
2023-05-31 17:02:45 +0900

    pjt finished 끝

--- js/sale.js
+++ js/sale.js
@@ -16,57 +16,50 @@ const addEvents = () => {
     s_no++;
     listView.style.display = "block";
     listTable();
+
+    document.querySelector("div.c_id input").value = "";
+    document.querySelector("div.p_no input").value = "";
+    document.querySelector("div.s_price input").value = "";
+
+    resetBtn.addEventListener("click", function () {
+      saleDB.clear();
+    });
   });
 };
 
 const listTable = () => {
-  let i = 0;
-
-  let sale = searchSale();
   //   console.log(product[1]);
+  let tbody = document.querySelector("tbody");
+  tbody.innerHTML = ``;
   saleDB.forEach(function (key, value) {
     let sale = key;
     let customer = searchCustomer(sale.c_id);
     let product = searchProduct(sale.p_no);
     let manufacture = searchManufacture(product.m_no);
 
-    document.querySelector("div.list_view tbody td:first-child").textContent =
-      customer.c_id;
-    document.querySelector("div.list_view tbody td:nth-child(2)").textContent =
-      customer.c_name;
-    document.querySelector("div.list_view tbody td:nth-child(3)").textContent =
-      customer.c_mail;
-    document.querySelector("div.list_view tbody td:nth-child(4)").textContent =
-      customer.c_phone;
-    document.querySelector("div.list_view tbody td:nth-child(5)").textContent =
-      customer.c_regDate;
-    document.querySelector("div.list_view tbody td:nth-child(6)").textContent =
-      product.p_no;
-    document.querySelector("div.list_view tbody td:nth-child(7)").textContent =
-      product.p_name;
-    document.querySelector("div.list_view tbody td:nth-child(8)").textContent =
-      product.p_price;
-    document.querySelector("div.list_view tbody td:nth-child(9)").textContent =
-      product.p_quantity;
-    document.querySelector("div.list_view tbody td:nth-child(10)").textContent =
-      product.p_reg_date;
-    document.querySelector("div.list_view tbody td:nth-child(11)").textContent =
-      manufacture.m_no;
-    document.querySelector("div.list_view tbody td:nth-child(12)").textContent =
-      manufacture.m_name;
-    document.querySelector("div.list_view tbody td:nth-child(13)").textContent =
-      manufacture.m_address;
-    document.querySelector("div.list_view tbody td:nth-child(14)").textContent =
-      manufacture.m_phone;
-    document.querySelector("div.list_view tbody td:nth-child(15)").textContent =
-      manufacture.m_manager;
-    document.querySelector("div.list_view tbody td:nth-child(16)").textContent =
-      manufacture.m_reg_date;
-    document.querySelector("div.list_view tbody td:nth-child(17)").textContent =
-      sale.s_no;
-    document.querySelector("div.list_view tbody td:nth-child(18)").textContent =
-      sale.s_price;
-    document.querySelector("div.list_view tbody td:nth-child(19)").textContent =
-      sale.s_reg_date;
+    tbody.innerHTML += `
+     <tr>
+              <td>${customer.c_id}</td>
+              <td>${customer.c_name}</td>
+              <td>${customer.c_mail}</td>
+              <td>${customer.c_phone}</td>
+              <td>${customer.c_regDate}</td>
+              <td>${product.p_no}</td>
+              <td>${product.p_name}</td>
+              <td>${product.p_price}</td>
+              <td>${product.p_quantity}</td>
+              <td>${product.p_reg_date}</td>
+              <td>${manufacture.m_no}</td>
+              <td>${manufacture.m_name}</td>
+              <td>${manufacture.m_address}</td>
+              <td>${manufacture.m_phone}</td>
+              <td>${manufacture.m_manager}</td>
+              <td>${manufacture.m_reg_date}</td>
+              <td>${sale.s_no}</td>
+              <td>${sale.s_price}</td>
+              <td>${sale.s_reg_date}</td>
+              
+    </tr>
+    `;
   });
 };
