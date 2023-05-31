040dcfdad782c8a08f4d0bb7a7720450244d67d8
beanbeeean <beanbeeean@naver.com>
2023-05-31 16:44:40 +0900

    완성

---
+++ js/index.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('DOM READY!!!!');
+
+    init();
+});
+
+
+const init = () => {
+    console.log('init() CALLED!!!');
+
+
+    addEvents();
+
+}
---
+++ js/utils.js
@@ -0,0 +1,16 @@
+let s_no = 0;
+
+const getTime = () => {
+  let today = new Date();
+
+  let year = today.getFullYear();
+  let month = today.getMonth() + 1;
+  let date = today.getDate();
+  let hour = today.getHours();
+  let minute = today.getMinutes();
+  let second = today.getSeconds();
+
+  return `${year}-${month}-${date} ${hour}:${minute}:${second}`;
+};
+
+let now = getTime();
